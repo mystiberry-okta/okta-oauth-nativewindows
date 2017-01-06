bb98269b7d0dbc519acc4280452c7cae15b30504
Raphael Londner <raphael.londner@okta.com>
2017-01-06 09:58:24 -0800

    Add project files.

---
+++ Okta.Samples.OpenIDConnect.NativeBrowserConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Okta OpenID Connect Console w/ Native Browser and Authorization Code Grant Flow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Okta, Inc.")]
+[assembly: AssemblyProduct("Okta.Samples.OpenIDConnect.NativeBrowserConsole")]
+[assembly: AssemblyCopyright("Copyright © Okta, Inc 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c7a91193-48d7-415b-82fd-61d65c21f672")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
